6ec5902d1fa38cc7aebbc4966f2758f29d0161e8
Alex <vlipoq@gmail.com>
2025-11-09 19:06:37 +0100

    test errors AI

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,12 @@
-function slugify(title) {
+const slugf = funcion (title) {
 
     const slug = title.toLowerCase().split(" ");
 
-    return slug.join("-");
+    return slug.join("_");
 }
 
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(slugf("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugf("English for developer")); // "english-for-developer"
+console.log(slugf("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugf("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
